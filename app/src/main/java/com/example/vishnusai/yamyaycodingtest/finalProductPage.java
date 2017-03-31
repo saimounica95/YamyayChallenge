226a283253ae22f6551464aa7e5b4b3419f4967f
vishnusait <vishnuf10@gmail.com>
2017-03-30 21:55:42 -0500

    Friday

--- app/src/main/java/com/example/vishnusai/yamyaycodingtest/finalProductPage.java
+++ app/src/main/java/com/example/vishnusai/yamyaycodingtest/finalProductPage.java
@@ -1,11 +1,15 @@
 package com.example.vishnusai.yamyaycodingtest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class finalProductPage extends AppCompatActivity {
     TextView product;
+    Button checkout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +18,17 @@ public class finalProductPage extends AppCompatActivity {
         product = (TextView)findViewById(R.id.finalProduct);
 
         product.setText(getIntent().getExtras().getString("productName"));
+
+        checkout =(Button)findViewById(R.id.checkout);
+
+        checkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(finalProductPage.this, checkoutPage.class);
+                startActivity(i);
+            }
+        });
+
 
     }
 }
